9bdd2b81f6476a08a55fd3be7dc40c7b96397867
PJ <petrusjohannesmaas@outlook.com>
2024-07-15 21:23:26 +0200

    added plugins up to autopairs

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,15 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local nvimtree = require("nvim-tree")
+
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        nvimtree.setup({})
+
+        vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
+
+    end,
+}
