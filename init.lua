f88bf7afb2ac38a7ae9851907163acc6daa628d0
PJ <petrusjohannesmaas@outlook.com>
2024-07-16 21:01:27 +0200

    finished tutorial but I have error with Mason

--- init.lua
+++ init.lua
@@ -16,11 +16,14 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-options")
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+    {import = "plugins.lsp" }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
