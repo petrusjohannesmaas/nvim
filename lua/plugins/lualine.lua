65afc9afc53abab09fdf7af8d11550b78cf9b154
Petrus Johannes Maas <petrusjohannesmaas@outlook.com>
2024-09-02 10:48:51 +0200

    Working on my Lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,17 +4,17 @@ return {
         require("lualine").setup({
             options = {
                 theme = "auto",
-                section_separators = { left = '', right = '' },
+                section_separators = { left = ' ✺  ', right = ' ✺  ' },
                 sections = {
                     lualine_a = {
-                        { 'mode', separator = { left = '' }, right_padding = 2 },
+                        { 'mode', separator = { left = '  ✺  ' }, right_padding = 3 },
                     },
                     lualine_b = { 'filename', 'branch' },
                     lualine_c = { 'fileformat' },
                     lualine_x = {},
                     lualine_y = { 'filetype', 'progress' },
                     lualine_z = {
-                        { 'location', separator = { right = '' }, left_padding = 2 },
+                        { 'location', separator = { right = '✺' }, left_padding = 3 },
                     },
                 }
             }
